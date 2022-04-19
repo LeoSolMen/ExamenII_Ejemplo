6fd33d7d3f14832a5a2686610ad4b876b81fadb7
Leo Solis <lsolis10885@ufide.ac.cr>
2022-04-18 20:31:14 -0600

    Examen 2 Ejemplo Html con bootstrap y carrusel de imagenes

---
+++ src/main/java/ExamenII_Ejemplo/ExamenII_Ejemplo/ExamenIiEjemploApplication.java
@@ -0,0 +1,13 @@
+package ExamenII_Ejemplo.ExamenII_Ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenIiEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenIiEjemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ExamenII_Ejemplo/ExamenII_Ejemplo/ExamenIiEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package ExamenII_Ejemplo.ExamenII_Ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenIiEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
